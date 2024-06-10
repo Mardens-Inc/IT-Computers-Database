a004eb6a9f7c2c301247551b1a63c560c9e1368a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-10 16:19:11 -0400

    Update Tailwind CSS plugin to support light and dark themes
    Tailwind CSS plugin configuration was updated to include color schemes for both light and dark themes. Each theme has been outlined with specific color codes for primary, secondary, and background visuals.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,26 @@ export default {
         extend: {},
     },
     darkMode: "class",
-    plugins: [nextui()]
+    plugins: [nextui({
+        themes: {
+            light: {
+                colors: {
+                    primary: {
+                        DEFAULT: "#f13848",
+                        foreground: "#fff",
+                    },
+                    secondary: "#32b0ff",
+                    background: "#f0f0f4",
+
+                }
+            },
+            dark: {
+                colors: {
+                    primary: "#ff3247",
+                    secondary: "#32b0ff",
+                    background: "#18181b",
+                }
+            },
+        }
+    })]
 }
