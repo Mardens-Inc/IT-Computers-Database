b9690e025f0b1de568e71c1b21eb058bedca1d96
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-10 16:19:23 -0400

    Add file reading and writing, refactor client.removeDir calls
    The commit includes the addition of readFileSync, writeFileSync, and readdirSync from the 'fs' module to handle .js files. Additionally, the deletion of directories and file removal is now wrapped in try/catch blocks to handle potential errors.

--- publish.js
+++ publish.js
@@ -1,4 +1,5 @@
 import {Client} from 'basic-ftp'
+import {readFileSync, writeFileSync, readdirSync} from 'fs'
 
 console.log("Uploading files")
 const client = new Client()
@@ -10,10 +11,32 @@ try {
         password: "ya5zMTzehxHN4AMW"
     })
 
+    const jsFiles = readdirSync("dist", {recursive: true}).filter(i => i.endsWith(".js"))
+
+    for (const file of jsFiles) {
+        const path = `dist/${file}`
+        const data = readFileSync(path, "utf8")
+        const newData = data.replace(/http:\/\/computers.local\//g, "/")
+        writeFileSync(path, newData, "utf8")
+    }
+
+
     // delete all files in the assets folder
-    await client.removeDir("assets")
-    await client.removeDir("api")
-    await client.remove("index.html")
+    try {
+        await client.removeDir("assets")
+    } catch (err) {
+        console.error(err)
+    }
+    try {
+        await client.removeDir("api")
+    } catch (err) {
+        console.error(err)
+    }
+    try {
+        await client.remove("index.html")
+    } catch (err) {
+        console.error(err)
+    }
 
     await client.uploadFromDir("dist", "/")
     await client.uploadFromDir("api", "/api")
