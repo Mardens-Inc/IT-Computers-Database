185b3423fb598968bbe4eea91fd30011ab964ca3
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-10 16:21:00 -0400

    Implement search functionality in Computers Database
    The new changes introduced a search bar into the application navbar. Now it's possible to search for computers in the computers table page, just by typing on it. The Home page is replaced by ComputerTable, which uses the search query to filter the table's items.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import $ from 'jquery'
-import {NextUIProvider} from "@nextui-org/react";
+import {Button, Input, NextUIProvider, Tooltip} from "@nextui-org/react";
 
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 
 import "./assets/scss/index.scss"
-import Home from "./assets/pages/Home.tsx";
 import ThemeSwitcher, {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
+import {SearchIcon} from "./assets/components/Icons.tsx";
+import {ComputersTable} from "./assets/pages/ComputersTable.tsx";
 
 applyTheme()
 
@@ -23,12 +24,13 @@ ReactDOM.createRoot($("#root")[0]!).render(
 
 function PageContent() {
     const navigate = useNavigate();
+    const [search, setSearch] = React.useState<string>("");
     return (
         <NextUIProvider navigate={navigate}>
-            <Nav/>
+            <Nav onSearch={setSearch}/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Home/>}/>
+                    <Route path="/" element={<ComputersTable search={search}/>}/>
                 </Route>
             </Routes>
         </NextUIProvider>
@@ -36,14 +38,22 @@ function PageContent() {
 }
 
 
-function Nav() {
+interface NavProps {
+    onSearch: (value: string) => void,
+}
+
+function Nav({onSearch}: NavProps) {
     return (
-        <Navbar>
+        <Navbar className="max-w-full">
             <NavbarContent>
                 <NavbarBrand>
                     <p className="font-bold text-inherit">IT Computers Database</p>
                 </NavbarBrand>
             </NavbarContent>
+
+            <Input placeholder="Type to search..." startContent={<SearchIcon size={16}/>} type="search" size="sm" classNames={{base: "max-w-full sm:max-w-[10rem h-10"}} onInput={(e) => {
+                onSearch(e.currentTarget.value);
+            }}/>
 
             <NavbarContent justify="end">
                 <NavbarItem>
