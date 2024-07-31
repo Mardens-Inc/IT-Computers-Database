0eca24c5881183ba2280299b836d00b2b2a984b5
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-31 14:21:24 -0400

    Remove Navbar and search functionality
    This commit removes the Navbar and associated search functionality from `main.tsx`. The `Nav` component and related imports have been deleted, simplifying the page content setup. The `ComputersTable` component no longer takes a search prop.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import {Input, NextUIProvider} from "@nextui-org/react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
-import ThemeSwitcher, {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
-import {SearchIcon} from "./assets/components/Icons.tsx";
+import {NextUIProvider} from "@nextui-org/react";
+import {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
 import {ComputersTable} from "./assets/pages/ComputersTable.tsx";
 import $ from "jquery";
 
@@ -24,45 +22,13 @@ ReactDOM.createRoot($("#root")[0]!).render(
 function PageContent()
 {
     const navigate = useNavigate();
-    const [search, setSearch] = React.useState<string>("");
     return (
         <NextUIProvider navigate={navigate}>
-            <Nav onSearch={setSearch}/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<ComputersTable search={search}/>}/>
+                    <Route path="/" element={<ComputersTable/>}/>
                 </Route>
             </Routes>
         </NextUIProvider>
-    );
-}
-
-
-interface NavProps
-{
-    onSearch: (value: string) => void,
-}
-
-function Nav({onSearch}: NavProps)
-{
-    return (
-        <Navbar className="max-w-full">
-            <NavbarContent>
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">IT Computers Database</p>
-                </NavbarBrand>
-            </NavbarContent>
-
-            <Input placeholder="Type to search..." startContent={<SearchIcon size={16}/>} type="search" size="sm" classNames={{base: "max-w-full sm:max-w-[10rem h-10"}} onInput={(e) =>
-            {
-                onSearch(e.currentTarget.value);
-            }}/>
-
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    <ThemeSwitcher/>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
     );
 }
